7beb369bd1e8982cef755de1cf9ef74eba9535d6
Fenet Damena <fenetdamena74@gmail.com>
2025-02-19 16:01:38 +0300

    Created 'Team' by A2SV Hub

---
+++ 19-Feb-2025/Team 273700.py
@@ -0,0 +1,12 @@
+# Problem: Team - https://codeforces.com/contest/231/problem/A
+
+n=int(input())
+c=0
+for i in range(n):
+    p,v,t=map(int,input().split())
+    a=p+v+t
+    if a>=2:
+        c+=1
+print (c)
+
+    
