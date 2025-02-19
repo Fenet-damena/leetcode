7369f30482e72f89ff62729e44e0ecd8a1c7a69b
Fenet Damena <fenetdamena74@gmail.com>
2025-02-19 10:02:04 +0300

    Created 'Chat Order' by A2SV Hub

---
+++ 19-Feb-2025/Chat Order 273018.py
@@ -0,0 +1,10 @@
+# Problem: Chat Order - https://codeforces.com/problemset/problem/637/B
+
+order={}
+for i in range(int(input())):
+    name=input()
+    if name in order:
+        del order[name]
+    order[name]=i
+for k in list(order.keys())[::-1]:
+    print(k)
