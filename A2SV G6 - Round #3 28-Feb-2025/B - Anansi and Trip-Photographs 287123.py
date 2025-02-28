04337dbe12d7df87e72e154bd6edbe401dd97cb7
Fenet Damena <fenetdamena74@gmail.com>
2025-02-28 19:35:54 +0300

    Created 'B - Anansi and Trip-Photographs' by A2SV Hub

---
+++ A2SV G6 - Round #3 28-Feb-2025/B - Anansi and Trip-Photographs 287123.py
@@ -0,0 +1,19 @@
+# Problem: B - Anansi and Trip-Photographs - https://codeforces.com/gym/588468/problem/B
+
+for i in range(int(input())):
+    n,x=map(int,input().split())
+    l=list(map(int,input().split()))
+    l.sort()
+    m=len(l)//2
+    l1=l[:m]
+    l2=l[m:]
+    t=True
+    for i in range(len(l1)):
+        if l2[i]-l1[i]<x:
+            t=False
+            break
+    if t:
+        print("YES")
+    else:
+        print("NO")
+    
