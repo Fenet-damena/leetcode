5bb067399ac29b5211e42f3dad9e3fe68fc57546
Fenet Damena <fenetdamena74@gmail.com>
2025-03-05 23:51:37 +0300

    Created 'Odd Subarrays' by A2SV Hub

---
+++ 05-Mar-2025/Odd Subarrays 293710.py
@@ -0,0 +1,15 @@
+# Problem: Odd Subarrays - https://codeforces.com/problemset/problem/1686/B
+
+for i in range(int(input())):
+    n = int(input())
+    num = list(map(int,input().split()))
+    ans = 0
+
+    left = 0
+    while left+1 < n:
+        if num[left] > num[left+1]:
+            ans += 1
+            left +=1
+        left +=1
+    print(ans)
+       
