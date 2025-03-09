7cb5ce06c7592fa621327ccf6cf66c5469b7449b
Fenet Damena <fenetdamena74@gmail.com>
2025-03-09 19:31:47 +0300

    Created 'B - Fix the Forecast!' by A2SV Hub

---
+++ A2SV G6 - Round #5 09-Mar-2025/B - Fix the Forecast! 298723.py
@@ -0,0 +1,16 @@
+# Problem: B - Fix the Forecast! - https://codeforces.com/gym/591928/problem/B
+
+for _ in range(int(input())):
+    n, k = map(int, input().split())  
+    a = list(map(int, input().split()))  
+    b = list(map(int, input().split()))  
+
+    indexed_a = sorted(enumerate(a), key=lambda x: x[1])
+    b.sort()  
+
+    result = [0] * n
+
+    for i in range(n):
+        original_index = indexed_a[i][0]
+        result[original_index] = b[i]
+    print(" ".join(map(str, result)))
