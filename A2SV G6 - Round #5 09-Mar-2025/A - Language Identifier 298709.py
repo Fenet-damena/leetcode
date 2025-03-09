2bce382f48846f0cc73e7c3148941c10afcc586f
Fenet Damena <fenetdamena74@gmail.com>
2025-03-09 18:58:10 +0300

    Created 'A - Language Identifier' by A2SV Hub

---
+++ A2SV G6 - Round #5 09-Mar-2025/A - Language Identifier 298709.py
@@ -0,0 +1,12 @@
+# Problem: A - Language Identifier - https://codeforces.com/gym/591928/problem/A
+
+for i in range(int(input())):
+    x = input()
+    n= len(x)
+    if x[n-2:] == "po":
+        print("FILIPINO")
+    elif x[n-4:] == "desu" or x[n-4:] == "masu":
+        print("JAPANESE")
+    else:
+        if x[n-5:] == "mnida":
+            print("KOREAN")
