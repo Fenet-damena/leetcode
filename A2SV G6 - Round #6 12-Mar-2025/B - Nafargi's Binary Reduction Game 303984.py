a3a096f9b6754c69d778fbc729707eee9bed7e2d
Fenet Damena <fenetdamena74@gmail.com>
2025-03-12 17:28:56 +0300

    Created 'B - Nafargi's Binary Reduction Game' by A2SV Hub

---
+++ A2SV G6 - Round #6 12-Mar-2025/B - Nafargi's Binary Reduction Game 303984.py
@@ -0,0 +1,12 @@
+# Problem: B - Nafargi's Binary Reduction Game - https://codeforces.com/gym/594077/problem/B
+
+n = int(input())
+s = input()
+stack = []
+
+for i in s:
+    if stack and stack[-1] != i:
+        stack.pop()
+    else:
+        stack.append(i)
+print(len(stack))
