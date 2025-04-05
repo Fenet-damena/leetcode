032966a6d6818bd1697f99123b2c70f7dd3955f7
Fenet Damena <fenetdamena74@gmail.com>
2025-04-05 01:37:37 -1200

    Created 'Linked List Cycle II' by A2SV Hub

---
+++ 05-Apr-2025/Linked List Cycle II 326821.py
@@ -0,0 +1,18 @@
+# Problem: Linked List Cycle II - https://leetcode.com/problems/linked-list-cycle-ii/
+
+class Solution(object):
+    def detectCycle(self, head):
+        slow = head
+        fast = head
+
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            if fast == slow:
+                break
+        else:
+            return None
+        while head != slow:
+            head = head.next
+            slow = slow.next
+        return head
