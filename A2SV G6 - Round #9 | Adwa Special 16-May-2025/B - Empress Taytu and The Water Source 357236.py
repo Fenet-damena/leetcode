01797398bc2e233bff2b51550459a5921c12667f
Fenet Damena <fenetdamena74@gmail.com>
2025-05-16 06:45:45 -1200

    Created 'B - Empress Taytu and The Water Source' by A2SV Hub

---
+++ A2SV G6 - Round #9 | Adwa Special 16-May-2025/B - Empress Taytu and The Water Source 357236.py
@@ -0,0 +1,37 @@
+# Problem: B - Empress Taytu and The Water Source - https://codeforces.com/gym/601269/problem/B
+
+from math import ceil
+no_g = int(input())
+t = []
+for _ in range(no_g):
+    n, k = map(int,input().split())
+    d = list(map(int,input().split()))
+    a =  list(map(int,input().split()))
+    t.append((n,k,d,a))
+    
+
+    
+def wagsize (no_g , t):
+    ans = []
+    for n, k ,d , a in t:
+        left, right = 1, max(d)
+        best = -1
+        while left <= right:
+            mid = (left + right)//2
+            times = 0
+            for  i in range(n):
+                rq = (d[i]+mid-1)//mid
+                times += rq * a[i]
+            if times <= k:
+                best = mid
+                right = mid -1
+            else:
+                left = mid + 1
+        ans. append(best)
+    return ans
+for i in wagsize(no_g,t):
+    print(i)
+
+
+
+    
