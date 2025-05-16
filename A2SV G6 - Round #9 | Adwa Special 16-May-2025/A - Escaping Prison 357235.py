68b8bf04982c295cd9785517cf3f13fa5381c8ab
Fenet Damena <fenetdamena74@gmail.com>
2025-05-16 06:45:42 -1200

    Created 'A - Escaping Prison' by A2SV Hub

---
+++ A2SV G6 - Round #9 | Adwa Special 16-May-2025/A - Escaping Prison 357235.py
@@ -0,0 +1,20 @@
+# Problem: A - Escaping Prison - https://codeforces.com/gym/601269/problem/A
+
+for j in range(int(input())):
+    n, h = map(int,input().split())
+    ll =[]
+    for i in range(n):
+        l,w = map(int,input().split())
+        ll.append([l,w])
+    cur = 0
+    for i,j  in ll:
+        if i >j:
+            cur += i
+        else:
+            cur += j
+    if cur >= h:
+        print("YES")
+    else:
+        print("NO")
+
+
