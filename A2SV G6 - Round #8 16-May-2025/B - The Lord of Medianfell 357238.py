56c8f13cad84dd12167b10a9c8555beb7047931c
Fenet Damena <fenetdamena74@gmail.com>
2025-05-16 06:46:22 -1200

    Created 'B - The Lord of Medianfell' by A2SV Hub

---
+++ A2SV G6 - Round #8 16-May-2025/B - The Lord of Medianfell 357238.py
@@ -0,0 +1,6 @@
+# Problem: B - The Lord of Medianfell - https://codeforces.com/gym/599383/problem/B
+
+for i in range(int(input())):
+    n, s= map(int,input().split())
+    maxm = s//(n//2 +1 )
+    print(maxm)
