4471fe7b5c9f2b79e5ca5accd7ca03433e176861
Fenet Damena <fenetdamena74@gmail.com>
2025-05-16 06:46:15 -1200

    Created 'A - The Mad King's Calls' by A2SV Hub

---
+++ A2SV G6 - Round #8 16-May-2025/A - The Mad King's Calls 357237.py
@@ -0,0 +1,9 @@
+# Problem: A - The Mad King's Calls - https://codeforces.com/gym/599383/problem/A
+
+for i in range(int(input())):
+    n = input()
+    b = int(n[0])-1
+    k = b*10
+    for i in range(1,len(n)+1):
+        k += i
+    print(k)
