c4a0384b1247b1081385d3fe5f987009a7014849
Fenet Damena <fenetdamena74@gmail.com>
2025-06-22 08:52:49 -1200

    Created 'Subsets' by A2SV Hub

---
+++ 22-Jun-2025/Subsets 369191.py
@@ -0,0 +1,20 @@
+# Problem: Subsets - https://leetcode.com/problems/subsets/
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        m = len(nums)
+       
+        ans = []
+        
+       
+       
+        for i in range(1 << m):
+            subset = []
+            for j in range(m):
+                if i &(1 << j):
+                    
+                    subset.append(nums[j])
+           
+            ans.append(subset)
+         
+        return ans
