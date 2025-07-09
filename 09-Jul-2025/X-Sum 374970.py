3967b9c0bfec52e2e1363841266ef54ed1861a7b
Fenet Damena <fenetdamena74@gmail.com>
2025-07-10 00:19:14 +0300

    Created 'X-Sum' by A2SV Hub

---
+++ 09-Jul-2025/X-Sum 374970.py
@@ -0,0 +1,18 @@
+# Problem: X-Sum - https://codeforces.com/problemset/problem/1676/D
+
+for _ in range(int(input())):
+    n,m = map(int,input().split())
+    b = [list(map(int,input().split())) for i in range(n)]
+
+    d1 = {}
+    d2 = {}
+
+    for i in range(n):
+        for j in range(m):
+            d1[i-j] = d1.get(i-j, 0) + b[i][j]
+            d2[i+j] = d2.get(i+j, 0) + b[i][j]
+    ans = 0
+    for i in range(n):
+        for j in range(m):
+            ans = max(ans, d1[i-j] + d2[i+j] - b[i][j])
+    print(ans)
