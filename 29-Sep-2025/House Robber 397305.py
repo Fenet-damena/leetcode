c9996a04a4394a413f342ad1cc710266df8da008
Fenet Damena <fenetdamena74@gmail.com>
2025-09-29 13:06:01 +0300

    Created 'House Robber' by A2SV Hub

---
+++ 29-Sep-2025/House Robber 397305.py
@@ -0,0 +1,19 @@
+# Problem: House Robber - https://leetcode.com/problems/house-robber/
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        memo = {}
+
+        def dp(i):
+            if i == 0:
+                return nums[0]
+            if i == 1:
+                return max(nums[0], nums[1])
+            if i not in memo:
+                rob = dp(i-2) + nums[i]
+                not_rob = dp(i-1)
+                memo[i] = max(rob, not_rob)
+            return memo[i]
+        return dp(len(nums)-1)
+
+        
