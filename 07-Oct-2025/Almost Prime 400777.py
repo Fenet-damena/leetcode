58f9160ae5e3528ac36a1144c4fafbf2ca282ad1
Fenet Damena <fenetdamena74@gmail.com>
2025-10-07 23:26:41 +0300

    Created 'Almost Prime' by A2SV Hub

---
+++ 07-Oct-2025/Almost Prime 400777.py
@@ -0,0 +1,26 @@
+# Problem: Almost Prime - https://codeforces.com/problemset/problem/26/A
+
+from collections import Counter
+ 
+ 
+n = int(input())
+def check(n):
+    fact = Counter()
+ 
+    d = 2
+ 
+    while d*d <= n:
+        while n % d == 0:
+                fact [d] += 1
+                n //= d
+        d += 1
+    if n>1:
+        fact[n] += 1
+    return len(fact)
+ 
+ 
+c = 0
+for i in range(1, n+1):
+    if check(i) == 2:
+        c += 1
+print(c)
