5e08927d25bdad3505953c2d18390f3c3235d511
Fenet Damena <fenetdamena74@gmail.com>
2025-11-02 04:53:17 -1200

    Created 'Swap Nodes in Pairs' by A2SV Hub

---
+++ 02-Nov-2025/Swap Nodes in Pairs 406987.py
@@ -0,0 +1,29 @@
+# Problem: Swap Nodes in Pairs - https://leetcode.com/problems/swap-nodes-in-pairs/description/
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(0)
+        dummy.next = head
+        prev = dummy
+
+        while head and head.next:
+            frist = head
+            second = head.next
+
+            prev.next = second
+            frist.next = second.next
+            second.next = frist
+            
+
+            head = frist.next
+            prev = frist
+
+        return dummy.next
+
+
+        
