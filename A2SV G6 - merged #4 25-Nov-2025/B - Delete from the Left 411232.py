687f8eaaa67bb69d41046faa85408f90d5a7f71b
Fenet Damena <fenetdamena74@gmail.com>
2025-11-24 21:58:46 -1200

    Created 'B - Delete from the Left' by A2SV Hub

---
+++ A2SV G6 - merged #4 25-Nov-2025/B - Delete from the Left 411232.py
@@ -0,0 +1,13 @@
+# Problem: B - Delete from the Left - https://codeforces.com/gym/653030/problem/B
+
+s = input().strip()
+t = input().strip()
+i, j = len(s) - 1, len(t) - 1
+c = 0
+
+while i >= 0 and j >= 0 and s[i] == t[j]:
+    c += 1
+    i -= 1
+    j -= 1
+ans= (len(s) - c) + (len(t) - c)
+print(ans)
