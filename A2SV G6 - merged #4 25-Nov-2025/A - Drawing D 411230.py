568680f9bbc90115e51651c7a58f545361348ff1
Fenet Damena <fenetdamena74@gmail.com>
2025-11-24 21:03:21 -1200

    Created 'A - Drawing D' by A2SV Hub

---
+++ A2SV G6 - merged #4 25-Nov-2025/A - Drawing D 411230.py
@@ -0,0 +1,12 @@
+# Problem: A - Drawing D - https://codeforces.com/gym/653030/problem/A
+
+n = int(input())
+
+mid = n // 2 
+
+for i in range(n):
+    leng = abs(mid - i)
+    Ds= n - 2 * leng
+    starts = leng
+    ans = '*' * starts + 'D' * Ds+ '*' * starts
+    print(ans)
