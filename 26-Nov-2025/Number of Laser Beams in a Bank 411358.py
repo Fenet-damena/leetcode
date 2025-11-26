570f8aaf4ab34f70b61c1e73bb785bddd29103d8
Fenet Damena <fenetdamena74@gmail.com>
2025-11-26 05:22:07 -1200

    Created 'Number of Laser Beams in a Bank' by A2SV Hub

---
+++ 26-Nov-2025/Number of Laser Beams in a Bank 411358.py
@@ -0,0 +1,13 @@
+# Problem: Number of Laser Beams in a Bank - https://leetcode.com/problems/number-of-laser-beams-in-a-bank/
+
+class Solution:
+    def numberOfBeams(self, bank: List[str]) -> int:
+        prev = 0
+        total = 0
+        for row in bank:
+            count = row.count('1')
+            if count:
+                total += prev * count
+                prev = count
+        return total
+        
