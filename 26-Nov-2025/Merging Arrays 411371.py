dd9e7b36460278f9528bacab7a68d78243e9f7b1
Fenet Damena <fenetdamena74@gmail.com>
2025-11-26 08:12:14 -1200

    Created 'Merging Arrays' by A2SV Hub

---
+++ 26-Nov-2025/Merging Arrays 411371.py
@@ -0,0 +1,23 @@
+# Problem: Merging Arrays - https://codeforces.com/edu/course/2/lesson/9/1/practice/contest/307092/problem/A
+
+n, m = map(int,input().split())
+l = list(map(int,input().split()))
+k = list(map(int,input().split()))
+i, j = 0, 0
+ans = []
+while i < n and j < m:
+    if l[i] < k[j]:
+        ans.append(l[i])
+        i += 1
+
+    else:
+        ans.append(k[j])
+        j += 1 
+while i< n:
+    ans.append(l[i])
+    i += 1  
+while j < m:    
+    ans.append(k[j])
+    j += 1   
+print(*ans)
+
